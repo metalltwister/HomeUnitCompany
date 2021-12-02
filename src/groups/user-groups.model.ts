961fa70ee3d19f6a29cba6560953c78f8ca3589d
metalltwister <twister94tm@gmail.com>
2021-12-02 14:22:50 +0700

    update user-groups.model

--- src/groups/user-groups.model.ts
+++ src/groups/user-groups.model.ts
@@ -2,13 +2,8 @@ import { Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript
 import { User } from "src/users/users.model";
 import { Group } from "./groups.model";
 
-interface GroupCreationAttributes {
-  title: string
-  description?: string
-}
-
 @Table({ tableName: 'user_groups', createdAt: false, updatedAt: false })
-export class UserGroups extends Model<UserGroups, GroupCreationAttributes> {
+export class UserGroups extends Model<UserGroups> {
 
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number
